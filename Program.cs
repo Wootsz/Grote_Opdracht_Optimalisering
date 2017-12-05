80ce5532f387f4c4375274cae100ca5015f0e796
Wootsz <wouterbrozius@hotmail.com>
2017-12-05 12:06:38 +0100

    adjbshas.k\fnáml\s

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grote_Opdracht_Optimalisering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
